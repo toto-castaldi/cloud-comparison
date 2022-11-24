a04840b487ae4507ab18124466debd97d505d091
Antonio Toto Castaldi <toto.castaldi@gmail.com>
2022-11-24 16:45:59 +0100

    s3 trigger

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,21 +13,67 @@ provider "aws" {
   region  = "eu-central-1"
 }
 
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
 
-  bucket = "toto-castaldi-00"
-
+data "archive_file" "lambda_zip_file" {
+  type        = "zip"
+  output_path = "/tmp/lambda_zip_file.zip"
+  source {
+    content  = file("../src/lambda.py")
+    filename = "lambda.py"
+  }
 }
 
-module "lambda_function" {
-  source = "terraform-aws-modules/lambda/aws"
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda"
 
-  function_name = "serverless-logic"
-  description   = "Serverless Logic"
-  handler       = "lambda.lambda_handler"
-  runtime       = "python3.8"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
 
-  source_path = "../src"
-  
+resource "aws_lambda_permission" "allow_bucket" {
+  statement_id  = "AllowExecutionFromS3Bucket"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.serverless_logic.arn
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.bucket.arn
+}
+
+
+resource "aws_lambda_function" "serverless_logic" {
+  function_name     = "serverless_logic"
+  description       = "Serverless Logic"
+  handler           = "lambda.lambda_handler"
+  runtime           = "python3.8"
+  filename          = "${data.archive_file.lambda_zip_file.output_path}"
+  source_code_hash  = "${data.archive_file.lambda_zip_file.output_base64sha256}"
+  role              = aws_iam_role.iam_for_lambda.arn
+}
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = "toto-castaldi-00"
+}
+
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = aws_s3_bucket.bucket.id
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.serverless_logic.arn
+    events              = ["s3:ObjectCreated:*"]
+    filter_prefix       = "input/"
+    filter_suffix       = ".txt"
+  }
+
+  depends_on = [aws_lambda_permission.allow_bucket]
 }
